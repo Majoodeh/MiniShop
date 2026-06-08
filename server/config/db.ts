41755d2d587f0c007a7c8e64aac8dc2fa4efc6fa
Majoodeh <Majdhussein144@gmail.com>
2026-06-08 12:56:49 +0200

    fear: setup product module schema

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async () => {
+export const connectDB = async (): Promise<void> => {
   try {
-    // Connect to the database  
-    const conn : any = await mongoose.connect(process.env.MONGO_URI!);
+    // Connect to the database
+    const conn = await mongoose.connect(process.env.MONGO_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error : any) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); // 1 indicates failure and 0 indicates success 
-  }    }
+  } catch (error) {
+    if (error instanceof Error) {
+        console.error(`Error: ${error.message}`);
+    } else {
+        console.error('An unknown error occurred while connecting to the database.');
+     }}
+
+
+    process.exit(1); // 1 indicates failure and 0 indicates success
+  }
+
--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,16 @@
 import express from 'express';
-import dotenv from "dotenv";
-import {connectDB} from './config/db.js';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-
-
 app.listen(process.env.PORT, () => {
-    connectDB();
+  connectDB();
   console.log(`Server is running on port ${process.env.PORT}`);
 });
